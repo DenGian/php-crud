d8d3a5d44d477bbd1e6411e8a4b1401235311653
Marteko9000 <themarteko@protonmail.com>
2022-07-14 11:50:03 +0200

    vardumped the students

--- Controller/StudentPageController.php
+++ Controller/StudentPageController.php
@@ -7,8 +7,16 @@ class StudentPageController{
         $this->dbLoader = new DatabaseLoader();
     }
     public function render(array $GET, array $POST){
-
-
+        $lalaArray=[];
+        $lala = $this->dbLoader->getConnection()->query("SELECT * FROM students");
+        while ($row = $lala->fetch()){
+           $lalaArray[$row[0]] = [
+            'name' => $row[1],
+            'email' => $row[2],
+            'group_id' => $row[3]
+           ];
+        }
+        var_dump($lalaArray);
 
 
         require 'View/studentpage.php';
