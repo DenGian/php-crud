6c6738269f1d2764740af6604e53521415cef281
DenGian <mondelaers.ian@gmail.com>
2022-07-14 08:57:16 +0200

    add teacher class

--- Model/teacher.php
+++ Model/teacher.php
@@ -1,6 +1,23 @@
 <?php
+declare(strict_types=1);
 
 class teacher
 {
+    private string $name;
+    private string $email;
 
+    public function __construct(string $name, string $email)
+    {
+        $this->name=$name;
+        $this->email=$email;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    public function getEmail(): string
+    {
+        return $this->name;
+    }
 }
