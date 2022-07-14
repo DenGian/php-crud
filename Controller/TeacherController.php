a0a829315d2fbc80226118c32e38d1fb9c451a28
DenGian <mondelaers.ian@gmail.com>
2022-07-14 11:39:33 +0200

    fixed array issue

--- Controller/TeacherController.php
+++ Controller/TeacherController.php
@@ -3,16 +3,19 @@ declare(strict_types = 1);
 
 class TeacherController
 {
-    private DatabaseLoader $databaseLoader;
+    private DatabaseLoader $DBLoader;
 
-    public function __construct(databaseLoader $databaseLoader)
+    public function __construct()
     {
-        $this->databaseLoader=$databaseLoader;
+        $this->DBLoader= new DatabaseLoader();
     }
     public function render(array $GET, array $POST)
     {
-
-
+        $sqlTeacherTable= $this->DBLoader->getConnection()->query('SELECT * FROM coaches');
+        $teacherArray=[];
+        while($row=$sqlTeacherTable->fetch()){
+            $teacherArray[]=new Teacher($row[0], $row[1], $row[2]);
+        }
         require 'View/teacher.php';
     }
 }
