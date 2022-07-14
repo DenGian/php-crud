e0c4d736cf2463726646bdae0c6b38bdc186b02b
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-07-14 14:48:43 +0200

    changed getters names

--- Model/Group.php
+++ Model/Group.php
@@ -8,28 +8,30 @@ class group
     private string $location;
     private int $groupCoachId;
 
+
     public function __construct(int $id,string $name,int $groupCoachId,string $location)
     {
         $this->id = $id;
         $this->name = $name;
         $this->location = $location;
         $this->groupCoachId = $groupCoachId;
+
     }
 
 
-    public function getId(): int
+    public function getGroupId(): int
     {
         return $this->id;
     }
 
    
-    public function getName(): string
+    public function getGroupName(): string
     {
         return $this->name;
     }
 
 
-    public function getLocation(): string
+    public function getGroupLocation(): string
     {
         return $this->location;
     }
@@ -39,6 +41,8 @@ class group
     {
         return $this->groupCoachId;
     }
+
+
 
 
 }
