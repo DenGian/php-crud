4f1f4b599dcedc1e39134ad8f95d734255214d19
Marteko9000 <themarteko@protonmail.com>
2022-07-14 15:14:38 +0200

    added student name as link and direct to infopage with id as variable

--- View/studentpage.php
+++ View/studentpage.php
@@ -1,4 +1,8 @@
 <?php require 'View/includes/header.php'; ?>
 <p><a href="index.php">homepage</a></p>
 <h1>hallo wereldje</h1>
+<h2>students</h2>
+<?php foreach ($lalaArray as $student) : ?>
+   <p><a href=<?php echo "index.php?page=info&student=" . $student->getId()?>><?=$student->getName()?></a></p>
+<?php endforeach; ?>
 <?php require 'View/includes/footer.php'; ?>
