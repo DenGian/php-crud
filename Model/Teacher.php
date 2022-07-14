636b96081a5efd5f16633014baad478da1209396
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-07-14 14:49:47 +0200

    added parameters to constructor

--- Model/Teacher.php
+++ Model/Teacher.php
@@ -7,8 +7,9 @@ class Teacher
     private string $name;
     private string $email;
 
-    public function __construct(string $name, string $email)
+    public function __construct(int $id, string $name, string $email)
     {
+        $this->id=$id;
         $this->name=$name;
         $this->email=$email;
     }
