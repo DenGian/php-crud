549ed0fa4b87662562ec78d811a5691b2cf70e34
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-07-14 09:42:28 +0200

    added temporary test query to controller

--- Controller/GroupController.php
+++ Controller/GroupController.php
@@ -12,8 +12,11 @@ class GroupController
 
     public function render(array $GET, array$POST)
     {
-
-
+        $sqlGroupTable = $this->databaseLoader->getConnection()->query("SELECT * FROM groups");
+        $groupArray = [];
+        while ($row = $sqlGroupTable->fetch()) {
+            $groupArray[] = new group($row[0], $row[1], $row[2], $row[3]);
+        }
         require 'view/group_page.php';
     }
 
