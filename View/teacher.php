dc0869c84792d7be9d9166bb84013233f3b40259
DenGian <mondelaers.ian@gmail.com>
2022-07-14 14:25:59 +0200

    updated table

--- View/teacher.php
+++ View/teacher.php
@@ -1,22 +1,48 @@
 <?php require 'includes/header.php'; ?>
 
-<table>
+
+
+
+<table class="table table-striped table-dark">
+    <thead>
     <tr>
-        <th>Id</th>
-        <th>Name</th>
-        <th>Email</th>
+        <th scope="col">Id</th>
+        <th scope="col">Name</th>
+        <th scope="col">Email</th>
     </tr>
+    </thead>
+    <tbody>
     <?php
-    foreach ($teacherArray as $teacher){
-        $teacherId=$teacher->getId();
-        $teacherName=$teacher->getName();
-        $teacherEmail=$teacher->getEmail();
-        echo '<tr>' . '<td>' . $teacherId . '</td>' .
-            '<td>' . $teacherName .'</td>' .
-            '<td>' . $teacherEmail . '</td>' .
-            '</tr>';
-    }
+   foreach ($teacherArray as $teacher) {
+       $teacherId = $teacher->getId();
+       $teacherName = $teacher->getName();
+       $teacherEmail = $teacher->getEmail();
+       echo '<tr>' . '<td>' . $teacherId . '</td>' .
+           '<td>' . $teacherName . '</td>' .
+           '<td>' . $teacherEmail . '</td>' .
+           '</tr>';
+   }
     ?>
+</table>
+
+
+<!--<table>-->
+<!--    <tr>-->
+<!--        <th>Id</th>-->
+<!--        <th>Name</th>-->
+<!--        <th>Email</th>-->
+<!--    </tr>-->
+<!--    --><?php
+//    foreach ($teacherArray as $teacher){
+//        $teacherId=$teacher->getId();
+//        $teacherName=$teacher->getName();
+//        $teacherEmail=$teacher->getEmail();
+//        echo '<tr>' . '<td>' . $teacherId . '</td>' .
+//            '<td>' . $teacherName .'</td>' .
+//            '<td>' . $teacherEmail . '</td>' .
+//            '</tr>';
+//    }
+//    ?>
 
 </table>
 
