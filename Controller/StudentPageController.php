79724962e558de469fd74aa858aa86428bfc8f48
Marteko9000 <themarteko@protonmail.com>
2022-07-14 15:13:19 +0200

    added dbloader as param

--- Controller/StudentPageController.php
+++ Controller/StudentPageController.php
@@ -3,20 +3,18 @@ declare(strict_types = 1);
 class StudentPageController{
     private DatabaseLoader $dbLoader;
 
-    public function __construct(){
-        $this->dbLoader = new DatabaseLoader();
+    public function __construct(DatabaseLoader $dbLoader){
+        $this->dbLoader = $dbLoader;
     }
     public function render(array $GET, array $POST){
         $lalaArray=[];
         $lala = $this->dbLoader->getConnection()->query("SELECT * FROM students");
         while ($row = $lala->fetch()){
-           $lalaArray[$row[0]] = [
-            'name' => $row[1],
-            'email' => $row[2],
-            'group_id' => $row[3]
-           ];
+           $lalaArray[] = new Student($row[0],$row[1],$row[2],$row[3]);
         }
+
         var_dump($lalaArray);
+      //  var_dump($lalaArray);
 
 
         require 'View/studentpage.php';
