8cb580d2ef02b807fe6a8000e5a2fec24f88fa08
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-07-14 11:45:37 +0200

    added basic table to display grouptable

--- View/group_page.php
+++ View/group_page.php
@@ -1,20 +1,26 @@
 <?php require 'includes/header.php'?>
-<?php
-foreach ($groupArray as $group) {
-    $groupName = ucfirst($group->getName());
-    $groupId = $group->getId();
-    $groupLocation = $group->getLocation();
-    $groupCoachId = $group->getGroupCoachId();
-    var_dump($group);
-    echo "<br>";
-    var_dump($groupId);
-    echo "<br>";
-    var_dump($groupName);
-    echo "<br>";
-    var_dump($groupLocation);
-    echo "<br>";
-    var_dump($groupCoachId);
-    echo "<br>";
-}
-?>
+
+<table>
+    <tr>
+        <th>Group ID</th>
+        <th>Name</th>
+        <th>Location</th>
+        <th>Teacher</th>
+    </tr>
+    <?php
+    foreach ($groupArray as $group) {
+        $groupName = ucfirst($group->getName());
+        $groupId = $group->getId();
+        $groupLocation = $group->getLocation();
+        $groupCoachId = $group->getGroupCoachId();
+
+        echo '<tr>';
+        echo '<td>' . $groupId;
+        echo '<td>' . $groupName;
+        echo '<td>' . $groupLocation;
+        echo '<td>' . $groupCoachId;
+        echo '</tr>';
+    }
+    ?>
+</table>
 <?php require 'includes/footer.php'?>
