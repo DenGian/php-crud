045c2b94e11b70e1f8099b4c40042d292ac58194
Marteko9000 <themarteko@protonmail.com>
2022-07-14 09:57:28 +0200

    added new dblaoder

--- Model/Student.php
+++ Model/Student.php
@@ -1,12 +1,14 @@
 <?php declare(strict_types = 1);
-class Student extends Coach{
+class Student extends Teacher{
     private int $id;
     private string $name;
     private string $email;
     private int $group_id;
+    private DatabaseLoader $DBLoader;
 
     public function __construct(int $group_id){
         $this->group_id = $group_id;
+        $this->DBLoader = new DatabaseLoader();
         parent::__construct($id, $name, $email);
     }
 
