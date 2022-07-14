8c870677233f2ad5c5f3ceea5cbd97a791420ca6
Marteko9000 <themarteko@protonmail.com>
2022-07-14 15:13:49 +0200

    deleted getetrs that are already in the teachers class

--- Model/Student.php
+++ Model/Student.php
@@ -3,25 +3,23 @@ class Student extends Teacher{
     private int $id;
     private string $name;
     private string $email;
-    private int $group_id;
-    private DatabaseLoader $DBLoader;
+    private ?int $group_id;
 
-    public function __construct(int $id, string $name, string $email, int $group_id){
+    public function __construct(int $id, string $name, string $email, ?int $group_id){
         parent::__construct($id, $name, $email);
         $this->group_id = $group_id;
-        $this->DBLoader = new DatabaseLoader();
        
     }
 
-    public function getId(): int {
-        return $this->id;
-        }
-    public function getName(): string {
-        return $this->name;
-        }
-    public function getEmail(): string {
-        return $this->email;
-        }
+    // public function getId(): int {
+    //     return $this->id;
+    //     }
+    // public function getName(): string {
+    //     return $this->name;
+    //     }
+    // public function getEmail(): string {
+    //     return $this->email;
+    //     }
     public function getGroupId(): int {
         return $this->group_id;
         }
