97882c42047cd03a82284003e919c0416206be43
DenGian <mondelaers.ian@gmail.com>
2022-07-15 14:27:57 +0200

    add buttons

--- View/teacher.php
+++ View/teacher.php
@@ -1,6 +1,5 @@
 <?php require 'includes/header.php'; ?>
 
-
 <table class="table table-striped table-dark">
     <thead>
     <tr>
@@ -15,13 +14,16 @@
         $teacherId = $teacher->getId();
         $teacherName = $teacher->getName();
         $teacherEmail = $teacher->getEmail();
-        $
         echo'<tr>' . '<td>' . $teacherId . '</td>' .
             '<td>' . $teacherName . '</td>' .
             '<td>' . $teacherEmail . '</td>' .
+            '<td><form method="post"><button class="btn-btn-primary" name="student" value="'.$teacherId.'">Show students</button></form></td>' .
+            '<td><form method="post"><button class="btn-btn-primary" name="edit" value="'.$teacherId.'">Edit</button></form></td>' .
+            '<td><form method="post"><button class="btn-btn-primary" name="delete" value="'.$teacherId.'">Delete</button></form></td>' .
             '</tr>';
     }
     ?>
 </table>
 
+<?php //var_dump($POST['student']); ?>
 <?php require 'includes/footer.php'; ?>
