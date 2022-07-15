f594c68055b853be8d1ef7f36ac668426d0332d3
DenGian <mondelaers.ian@gmail.com>
2022-07-15 14:25:53 +0200

    add student class for functionality

---
+++ Model/Student.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types = 1);
+class Student extends Teacher{
+    private int $id;
+    private string $name;
+    private string $email;
+    private ?int $group_id;
+
+    public function __construct(int $id, string $name, string $email, ?int $group_id){
+        parent::__construct($id, $name, $email);
+        $this->group_id = $group_id;
+
+    }
+
+    // public function getId(): int {
+    //     return $this->id;
+    //     }
+    // public function getName(): string {
+    //     return $this->name;
+    //     }
+    // public function getEmail(): string {
+    //     return $this->email;
+    //     }
+    public function getGroupId(): int {
+        return $this->group_id;
+        }
+}
