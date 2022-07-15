ee048dac5bea450d630532bef1b40fe00093cc31
Martekode <BrianDesmedt9000@hotmail.com>
2022-07-15 17:19:47 +0200

    updated the view accordingly to the other files changes

--- View/editstudentpage.php
+++ View/editstudentpage.php
@@ -4,12 +4,12 @@
 <?= $GET['student'] ?>
 <form method="POST">
     <h2>ID: <?= $sStudentId ?? "id" ?></h2>
-    <input type="text" placeholder="<?= $sStudentId ?? "id" ?>">
     <h2>NAME: <?= $sStudentName ?? "name" ?></h2>
-    <input type="text" placeholder="<?= $sStudentName ?? "name" ?>">
+    <input name="newName" type="text" placeholder="<?= $sStudentName ?? "name" ?>">
     <h2>EMAIL: <?= $sStudentEmail ?? "email" ?></h2>
-    <input type="text" placeholder="<?= $sStudentEmail ?? "email" ?>">
+    <input name="newEmail" type="text" placeholder="<?= $sStudentEmail ?? "email" ?>">
     <h2>GROUP_ID: <?= $sStudentGroupId ?? "group_id" ?></h2>
-    <input type="text" placeholder="<?= $sStudentGroupId ?? "group_id" ?>">
+    <input name="newGroupId" type="text" placeholder="<?= $sStudentGroupId ?? "group_id" ?>">
+    <br><button style="background-color:green;color:white;border-radius:10px; border-color:green;" name="updateStudent">UPDATE</button>
 </form>
 <?php require 'includes/footer.php' ?>
