caa75127ccc4df976e3118cd753452e19d8f2987
Martekode <BrianDesmedt9000@hotmail.com>
2022-07-15 11:56:03 +0200

    added delete logic

--- Controller/StudentPageController.php
+++ Controller/StudentPageController.php
@@ -1,20 +1,27 @@
 <?php
-declare(strict_types = 1);
-class StudentPageController{
+
+declare(strict_types=1);
+class StudentPageController
+{
     private DatabaseLoader $dbLoader;
 
-    public function __construct(DatabaseLoader $dbLoader){
+    public function __construct(DatabaseLoader $dbLoader)
+    {
         $this->dbLoader = $dbLoader;
     }
-    public function render(array $GET, array $POST){
-        $lalaArray=[];
+    public function render(array $GET, array $POST)
+    {
+        $lalaArray = [];
         $lala = $this->dbLoader->getConnection()->query("SELECT * FROM students");
-        while ($row = $lala->fetch()){
-           $lalaArray[] = new Student($row[0],$row[1],$row[2],$row[3]);
+        while ($row = $lala->fetch()) {
+            $lalaArray[] = new Student($row[0], $row[1], $row[2], $row[3]);
         }
 
         var_dump($lalaArray);
-      //  var_dump($lalaArray);
+        if (isset($POST['deleteStudent'])) {
+            $this->dbLoader->deleteStudent($POST['deleteStudent']);
+        }
+        //  var_dump($lalaArray);
 
 
         require 'View/studentpage.php';
