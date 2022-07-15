e0924b0144e57dac7b2d0492cf450099cad92312
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-07-15 09:22:26 +0200

    removed old code, added getAllGroups getter

--- Controller/GroupController.php
+++ Controller/GroupController.php
@@ -12,18 +12,8 @@ class GroupController
     }
     public function render(array $GET, array $POST):void
     {
-      
-        $sqlGroupTable = $this->DBLoader->getConnection()->query("SELECT * FROM `groups`");
-        $groupArray = [];
-        while ($row = $sqlGroupTable->fetch()) {
-            $groupArray[] = new group($row[0], $row[1], $row[2], $row[3]);
-        }
+        $groupArray =  $this->DBLoader->getAllGroups();
 
-            $sqlCoachName = $this->DBLoader->getConnection()->query("select * from coaches where id = (select coach_id from `groups` limit 1)");
-            $coachArray = [];
-            while ($row = $sqlCoachName->fetch()){
-                $coachArray[]= new Teacher($row[0], $row[1], $row[2]);
-            }
 
 
 
