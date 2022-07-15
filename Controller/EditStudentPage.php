8d11b6ad90edcafb7d438bf2716ccaa1fc20ecc9
Martekode <BrianDesmedt9000@hotmail.com>
2022-07-15 14:26:39 +0200

    added logic to display the info of the student

--- Controller/EditStudentPage.php
+++ Controller/EditStudentPage.php
@@ -10,6 +10,12 @@ class EditStudentPage
     }
     public function render(array $GET, array $POST)
     {
+        $selectedStudent = $this->dbLoader->getStudentById($GET['student']);
+        var_dump($selectedStudent);
+        $sStudentId = $selectedStudent[0]['id'];
+        $sStudentName = $selectedStudent[0]['name'];
+        $sStudentEmail = $selectedStudent[0]['email'];
+        $sStudentGroupId = $selectedStudent[0]['group_id'];
         require 'View/editstudentpage.php';
     }
 }
