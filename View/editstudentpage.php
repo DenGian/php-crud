312a184698fc8d4014f94a597632b823457a1e59
Martekode <BrianDesmedt9000@hotmail.com>
2022-07-15 14:25:39 +0200

    added overview of student with input fields

--- View/editstudentpage.php
+++ View/editstudentpage.php
@@ -2,4 +2,14 @@
 <a href="index.php">hompage</a>
 <h1>hello world</h1>
 <?= $GET['student'] ?>
+<form method="POST">
+    <h2>ID: <?= $sStudentId ?? "id" ?></h2>
+    <input type="text" placeholder="<?= $sStudentId ?? "id" ?>">
+    <h2>NAME: <?= $sStudentName ?? "name" ?></h2>
+    <input type="text" placeholder="<?= $sStudentName ?? "name" ?>">
+    <h2>EMAIL: <?= $sStudentEmail ?? "email" ?></h2>
+    <input type="text" placeholder="<?= $sStudentEmail ?? "email" ?>">
+    <h2>GROUP_ID: <?= $sStudentGroupId ?? "group_id" ?></h2>
+    <input type="text" placeholder="<?= $sStudentGroupId ?? "group_id" ?>">
+</form>
 <?php require 'includes/footer.php' ?>
