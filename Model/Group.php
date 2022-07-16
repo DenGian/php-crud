b96fa16ed7755e76f6d6104256567f07ddc00066
Martekode <BrianDesmedt9000@hotmail.com>
2022-07-16 21:51:05 +0200

    init group model

---
+++ Model/Group.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+class group
+{
+    private int $id;
+    private string $name;
+    private string $location;
+    private int $groupCoachId;
+
+
+    public function __construct(int $id, string $name, int $groupCoachId, string $location)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->location = $location;
+        $this->groupCoachId = $groupCoachId;
+    }
+
+
+    public function getGroupId(): int
+    {
+        return $this->id;
+    }
+
+
+    public function getGroupName(): string
+    {
+        return $this->name;
+    }
+
+
+    public function getGroupLocation(): string
+    {
+        return $this->location;
+    }
+
+
+    public function getGroupCoachId(): int
+    {
+        return $this->groupCoachId;
+    }
+}
