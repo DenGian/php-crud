5c7be1a5f35336d57a58ad58c9c90dcf7846be59
Martekode <BrianDesmedt9000@hotmail.com>
2022-07-16 19:45:18 +0200

    added the formfield with inputs and create button

--- View/createstudentpage.php
+++ View/createstudentpage.php
@@ -2,4 +2,15 @@
 <a style="color:red; font-weight:bold; border:1px solid red; text-decoration:none;" href="index.php">Homepage</a>
 <a style="color:purple; font-weight:bold; border:1px solid purple; text-decoration:none;" href="index.php?page=student">Student Page</a>
 <h1>hello world</h1>
+<p>here you can create a new student by filling in the forms and press the create button. Have fun!!</p>
+<form method="post">
+    <label for="name">Name</label>
+    <input type="text" name="name" id="name">
+    <label for="email">Email</label>
+    <input type="text" name="email" id="email">
+    <select name="group" id="group">
+        <option value="1">Lammar</option>
+    </select>
+    <button style="background-color:lightgreen; border:1px solid lightgreen; color:white; font-weight:bold;" name="submitNewStudent">Create!</button>
+</form>
 <?php require 'includes/footer.php' ?>
