e78255de98704b15a325a17a41d4ce81739e62fe
Martekode <BrianDesmedt9000@hotmail.com>
2022-07-16 21:49:47 +0200

    added logic for display of the fetched groups

--- View/createstudentpage.php
+++ View/createstudentpage.php
@@ -1,7 +1,7 @@
 <?php require 'includes/header.php' ?>
 <a style="color:red; font-weight:bold; border:1px solid red; text-decoration:none;" href="index.php">Homepage</a>
 <a style="color:purple; font-weight:bold; border:1px solid purple; text-decoration:none;" href="index.php?page=student">Student Page</a>
-<h1>hello world</h1>
+<h1 style="color:orange;">CREATE STUDENT</h1>
 <p>here you can create a new student by filling in the forms and press the create button. Have fun!!</p>
 <form method="post">
     <label for="name">Name</label>
@@ -9,7 +9,9 @@
     <label for="email">Email</label>
     <input type="text" name="email" id="email">
     <select name="group" id="group">
-        <option value="1">Lammar</option>
+        <?php foreach ($groupArray as $group) : ?>
+            <option value="<?= $group->getGroupId(); ?>"><?= $group->getGroupName(); ?></option>
+        <?php endforeach; ?>
     </select>
     <button style="background-color:lightgreen; border:1px solid lightgreen; color:white; font-weight:bold;" name="submitNewStudent">Create!</button>
 </form>
