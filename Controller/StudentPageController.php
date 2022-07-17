08da5f9f3778aee0f61870bd2cf5be94956d3c10
Martekode <BrianDesmedt9000@hotmail.com>
2022-07-17 13:17:45 +0200

    student table get refreshed after delete

--- Controller/StudentPageController.php
+++ Controller/StudentPageController.php
@@ -20,6 +20,11 @@ class StudentPageController
         #var_dump($lalaArray);
         if (isset($POST['deleteStudent'])) {
             $this->dbLoader->deleteStudent($POST['deleteStudent']);
+            $lalaArray = [];
+            $lala = $this->dbLoader->getConnection()->query("SELECT * FROM students");
+            while ($row = $lala->fetch()) {
+                $lalaArray[] = new Student($row[0], $row[1], $row[2], $row[3]);
+            }
         }
         //  var_dump($lalaArray);
 
