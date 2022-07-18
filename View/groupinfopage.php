e07ad7ff7ff0816b11568355a1beeb1eb497f76b
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-07-18 12:30:23 +0200

    added section to display all assigned students

--- View/groupinfopage.php
+++ View/groupinfopage.php
@@ -8,5 +8,11 @@
         <h2>GROUP LOCATION: <?= $group->getGroupLocation(); ?></h2>
         <h3>Assinged Coach</h3>
         <h2>COACH: <?= $teacherName; ?></h2>
+        <h3>Assigned students</h3>
+        <h2>Student names</h2>
+<!--        --><?php //var_dump($students);?>
+        <?php foreach($students as $student):?>
+            <h3><?=$student['firstname']?></h3>
+        <?php endforeach;?>
     </section>
 <?php require 'includes/footer.php' ?>
