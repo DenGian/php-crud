31f6a27e32df30e138e40450b23e978447e19816
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-07-18 12:07:02 +0200

    added page to display info of specific group

--- View/groupinfopage.php
+++ View/groupinfopage.php
@@ -1,7 +1,12 @@
-<?php
-require 'includes/header.php';
-?>
-
-<a href="index.php">Homepage</a>
-
-<?php require 'includes/footer.php'; ?>
+<?php require 'includes/header.php' ?>
+    <section>
+        <a style="color:red; font-weight:bold; text-decoration:none;border:1px solid red;" href="index.php">Back to homepage</a>
+        <a style="color:purple; font-weight:bold; text-decoration:none;border:1px solid purple;" href="index.php?page=groups">Group Page</a>
+        <h1 style="color:pink;">Group Info Page</h1>
+        <h2><?= $group->getGroupName() ?? 'group'; ?></h2>
+        <h2>GROUP ID: <?= $group->getGroupId(); ?></h2>
+        <h2>GROUP LOCATION: <?= $group->getGroupLocation(); ?></h2>
+        <h3>Assinged Coach</h3>
+        <h2>COACH: <?= $teacherName; ?></h2>
+    </section>
+<?php require 'includes/footer.php' ?>
