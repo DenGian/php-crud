d723efeccb7981ee5407f613a53ab168415d2e56
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-07-18 13:50:53 +0200

    removed unneccessary var dumps

--- Controller/EditStudentPage.php
+++ Controller/EditStudentPage.php
@@ -11,15 +11,13 @@ class EditStudentPage
     public function render(array $GET, array $POST)
     {
         $selectedStudent = $this->dbLoader->getStudentById($GET['student']);
-        //var_dump($selectedStudent);
         $sStudentId = $selectedStudent[0]['id'];
         $sStudentName = $selectedStudent[0]['firstname'];
         $sStudentEmail = $selectedStudent[0]['email'];
         $sStudentGroupId = $selectedStudent[0]['group_id'];
         $groupFetch = $this->dbLoader->getAllGroups();
-        //var_dump($groupFetch);
+
         if (isset($POST['updateStudent'])) {
-            //var_dump($POST['newName']);
             $sStudentName = $_POST['newName'];
             $sStudentEmail = $_POST['newEmail'];
             $sStudentGroupId = (int) $_POST['newGroupId'];
