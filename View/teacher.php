f286bcce92b7195f00b77b9e606314ec08d0b02a
DenGian <mondelaers.ian@gmail.com>
2022-07-18 14:41:38 +0200

    updated link

--- View/teacher.php
+++ View/teacher.php
@@ -20,29 +20,29 @@
                 '<td>' . $teacherEmail . '</td>' .
                 '<td><form method="post"><button class="btn btn-primary" name="student" value="' . $teacherId . '">Show students</button></form></td>' .
                 //'<td><form method="post"><button class="btn btn-primary" name="student" value="'.$teacherId.'">Show students</button></form></td>' .
-                '<td><form method ="post"> <button class="btn btn-warning" value="'.$teacherId .'"name="editteacher" style="padding: 0">  <a style="padding=1em; href="index.php?page=editteacherpage&teacher='.$teacherId .'">Edit</button></form></td>'.
+                '<td><form method ="post"> <button class="btn btn-warning" value="'.$teacherId .'"name="editteacher" style="padding: 0">  <a style="padding=1em;" href="index.php?page=editteacherpage&teacher='.$teacherId .'">Edit</button></form></td>'.
                 '<td><form method="post"><button class="btn btn-danger" name="delete" value="' . $teacherId . '">Delete</button></form></td>' .
                 '</tr>';
         }
         ?>
     </table>
 
-    <table class="table table-striped table-dark">
-        <thead>
-        <tr>
-            <th scope="col">Student name</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php
-        foreach ($requestedStudents as $requestedStudent) {
-            $studentName = $requestedStudent->getName();
-            echo '<tr>' .
-                '<td>' . $studentName .
-                '</td> </tr>';
-        }
-        ?>
-    </table>
+<!--    <table class="table table-striped table-dark">-->
+<!--        <thead>-->
+<!--        <tr>-->
+<!--            <th scope="col">Student name</th>-->
+<!--        </tr>-->
+<!--        </thead>-->
+<!--        <tbody>-->
+<!--        --><?php
+//        foreach ($requestedStudents as $requestedStudent) {
+//            $studentName = $requestedStudent->getName();
+//            echo '<tr>' .
+//                '<td>' . $studentName .
+//                '</td> </tr>';
+//        }
+//        ?>
+<!--    </table>-->
 
     <form method="post">
         <button class="btn btn-success" value = "createNewCoach" name =" create new"><a style="text-decoration:none; color:white; padding:1em" href="index.php?page=createteacher">Create new coach</button>
