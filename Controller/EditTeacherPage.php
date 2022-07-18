303ed33503472b33312c788fa45b6c4231e0d60d
DenGian <mondelaers.ian@gmail.com>
2022-07-18 14:39:38 +0200

    fixed issues with function

--- Controller/EditTeacherPage.php
+++ Controller/EditTeacherPage.php
@@ -10,20 +10,19 @@ class EditTeacherPage
     }
     public function render(array $GET, array $POST)
     {
-        $selectedCoach = $this->dbLoader->getCoachById($GET['coach']);
+        $selectedCoach = $this->dbLoader->getTeacherById($GET['teacher']);
         //var_dump($selectedStudent);
         $sCoachId = $selectedCoach[0]['id'];
         $sCoachName = $selectedCoach[0]['coach_name'];
         $sCoachEmail = $selectedCoach[0]['email'];
         //$sStudentGroupId = $selectedStudent[0]['group_id'];
-        $groupFetch = $this->dbLoader->getAllGroups();
         //var_dump($groupFetch);
         if (isset($POST['updateCoach'])) {
             //var_dump($POST['newName']);
             $sCoachName = $_POST['newName'];
             $sCoachEmail = $_POST['newEmail'];
             //$sStudentGroupId = (int) $_POST['newGroupId'];
-            $this->dbLoader->updateCoach($sCoachId, $sCoachName, $sCoachEmail);
+            $this->dbLoader->updateTeacher($sCoachId, $sCoachName, $sCoachEmail);
         }
         require 'View/editteacherpage.php';
     }
