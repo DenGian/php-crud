ed409b31be3bf84b621c7da404391011de406f6c
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-07-18 09:22:18 +0200

    added comments

--- Controller/GroupController.php
+++ Controller/GroupController.php
@@ -12,15 +12,15 @@ class GroupController
     }
     public function render(array $GET, array $POST):void
     {
-        $groupArray =  $this->DBLoader->getAllGroups();
+        $groupArray =  $this->DBLoader->getAllGroups(); //getting all info from the groups table
 
         //delete function
-        if(isset($POST['delete'])){
-            $this->DBLoader->deleteGroup($POST['delete']);
-            header('location:index.php?page='.$GET['page']);
+        if(isset($POST['delete'])){                     // if the delete button is pressed
+            $this->DBLoader->deleteGroup($POST['delete']); //$POST['delete'] has the value of the id of the selected group
+            header('location:index.php?page='.$GET['page']); // auto refresh page so deleted entry is gone
         }
 
-        require 'view/group_page.php';
+        require 'view/group_page.php'; // load in group pages
     }
 
 }
