9d14be1692a3a57ba9999172db48c73bcbca81a3
DenGian <mondelaers.ian@gmail.com>
2022-07-18 14:40:40 +0200

    fixed naming issues

--- View/createteacherpage.php
+++ View/createteacherpage.php
@@ -1,19 +1,14 @@
 <?php require 'includes/header.php' ?>
 <a style="color:red; font-weight:bold; border:1px solid red; text-decoration:none;" href="index.php">Homepage</a>
-<a style="color:purple; font-weight:bold; border:1px solid purple; text-decoration:none;" href="index.php?page=teacher">Teacher Page</a>
+<a style="color:purple; font-weight:bold; border:1px solid purple; text-decoration:none;" href="index.php?page=coach">Teacher Page</a>
 <h1 style="color:green;">CREATE TEACHER</h1>
 <p>here you can create a new coach by filling in the forms and press the create button. Have fun!!</p>
 <form method="post">
     <label for="name">Name</label>
-    <input type="text" name="name" id="name">
+    <input type="text" name="coach_name" id="name">
     <label for="email">Email</label>
     <input type="text" name="email" id="email">
-    <select name="group" id="group">
-        <?php foreach ($groupArray as $group) : ?>
-            <option value="<?= $group->getGroupId(); ?>"><?= $group->getGroupName(); ?></option>
-        <?php endforeach; ?>
-    </select>
-    <button style="background-color:lightgreen; border:1px solid lightgreen; color:white; font-weight:bold;" name="submitNewCoach">Create!</button>
+    <button style="background-color:lightgreen; border:1px solid lightgreen; color:white; font-weight:bold;" name="submitNewTeacher">Create!</button>
 </form>
 <p><?= $succes ?? 'STATUS'; ?></p>
 <?php require 'includes/footer.php' ?>
